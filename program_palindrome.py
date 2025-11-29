0eda0a500c0700eb6358fb548e0270a83c2beedd
gouri <smchikkeri5757@gmail.com>
2025-11-29 10:35:18 +0530

    Update program_palindrome.py

--- program_palindrome.py
+++ program_palindrome.py
@@ -5,7 +5,7 @@ if len(sys.argv)==3:
   string_name2 = sys.argv[2]
 else:
   string_name1 = "gadag"
-  string_name2 = "malayalam"
+  string_name2 = "men"
 if string_name1 == string_name1[::-1]:
   print(f"is a palindrome")
 else:
